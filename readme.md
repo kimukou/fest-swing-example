efa62de88fbc667ce02b150ee6d4d7c48c3aaf73
kimukou <kimukou.buzz@gmail.com>
2011-06-22 18:36:54 +0900

    gradle ver setting

---
+++ readme.md
@@ -0,0 +1,5 @@
+
+OpenCV for Java
+	javaCV http://code.google.com/p/javacv/
+	nativelibs4java http://code.google.com/p/nativelibs4java/wiki/OpenCV
+
---
+++ run.bat
@@ -0,0 +1,8 @@
+set GRADLE_HOME=D:\Tooldev\gradle-1.0-milestone-3
+set JAVA_HOME=c:\opt\jdk
+
+set PATH=%GRADLE_HOME%/bin;%JAVA_HOME%/bin
+
+
+call gradle test %1
+
