6b46e4ade9bdb273defa667ffeed3699f6d6a7fc
kimukou <kimukou.buzz@gmail.com>
2011-07-17 16:03:30 +0900

    update build.gradle

--- run_gui.bat
+++ run_gui.bat
@@ -1,8 +1,9 @@
-set GRADLE_HOME=D:\Tooldev\gradle-1.0-milestone-3
+::set GRADLE_HOME=D:\Tooldev\gradle-1.0-milestone-3
 set JAVA_HOME=c:\opt\jdk
 
 set PATH=%GRADLE_HOME%/bin;%JAVA_HOME%/bin
 
 
-call gradle --gui %1
+::call gradle --gui %1
+call gradlew --gui %1
 
