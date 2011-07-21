cc6b074831d39f99fdeb3367946f0d1d5337967f
kimukou <kimukou.buzz@gmail.com>
2011-07-21 20:04:12 +0900

    groovy version setting

---
+++ run_g.bat
@@ -0,0 +1,9 @@
+::set GRADLE_HOME=D:\Tooldev\gradle-1.0-milestone-3
+set JAVA_HOME=c:\opt\jdk
+
+set PATH=%GRADLE_HOME%/bin;%JAVA_HOME%/bin
+
+
+::call gradle test %1
+call gradlew -b build_g.gradle test %1
+
